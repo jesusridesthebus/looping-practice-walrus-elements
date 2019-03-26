54a02f358a985756917f2639c42c13b1118e5d29
Liz Kelley and Marina Poltorak <liz@>
2019-03-26 08:39:49 -0700

    add jq and clean up

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function() {
+    $("h1").click(function() {
+      alert("This is a header.");
+      alert("I told you this is a header!");
+    });
+
+    $("p").click(function() {
+      alert("This is a paragraph.");
+    });
+
+    $("img").click(function() {
+      alert("This is an image.");
+    });
+});
