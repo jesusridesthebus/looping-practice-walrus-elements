08000b854f698396dd21d103fbc75e00355cb8bc
Liz Kelley and Marina Poltorak <marina@>
2019-03-26 11:30:07 -0700

    add several code blocks of functions to get walrus and text to toggle and show/hide where appropriate. still working out a couple bugs

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,20 @@ $(document).ready(function() {
   $(".clickable").click(function() {
     $("#walrus-showing").fadeToggle();
     $("#walrus-hidden").slideToggle();
+    $("#text-showing").toggle();
+    $("#text-hidden").hide();
   });
+  $("#clickable-walrus").click(function() {
+    $("#walrus-showing").toggle();
+    $("#text-showing").toggle();
+    $("#walrus-hidden").show();
+  })
+  $("#first-clickable").click(function() {
+    $("#text-hidden").show();
+    $("#text-showing").fadeToggle();
+  });
+  $("#second-clickable").click(function(){
+    $("#text-hidden").fadeToggle();
+    $("#text-showing").show();
+  })
 });
